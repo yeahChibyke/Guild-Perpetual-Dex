f07730649591d164f8b34e7d76b78a507055f6ca
yeahChibyke <chibykepride@gmail.com>
2025-08-28 14:57:46 +0100

    added GuildToken.sol

--- src/GuildToken.sol
+++ src/GuildToken.sol
@@ -1,14 +1,57 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GuildToken is ERC20 {
-    IERC20 iUSDC;
+contract GuildToken is ERC20, Ownable {
+    error GTK__ZeroAddress();
+    error GTK__NotAdmin();
+    error GTK__NotVault();
+    // error GTK__VaultNotSet();
 
-    constructor(address _usdc) ERC20("GuildToken", "GTK") {
-        iUSDC = IERC20(_usdc);
+    event VaultSet(address indexed vault);
+
+    address public admin;
+    address public vault;
+
+    modifier onlyAdmin() {
+        if (msg.sender != admin) {
+            revert GTK__NotAdmin();
+        }
+        _;
     }
 
-    function deposit(uint256 _amount) external {}
+    modifier onlyVault() {
+        if (msg.sender != address(vault)) {
+            revert GTK__NotVault();
+        }
+        _;
+    }
+
+    constructor(address _admin) ERC20("GuildToken", "GTK") Ownable(_admin) {
+        if (_admin == address(0)) {
+            revert GTK__ZeroAddress();
+        }
+
+        admin = _admin;
+    }
+
+    function setVault(address _vault) external onlyAdmin {
+        if (_vault == address(0)) {
+            revert GTK__ZeroAddress();
+        }
+
+        vault = _vault;
+
+        emit VaultSet(vault);
+    }
+
+    function mint(address _to, uint256 _value) external onlyVault {
+        _mint(_to, _value);
+    }
+
+    function burn(address _from, uint256 _value) external onlyVault {
+        _burn(_from, _value);
+    }
 }
