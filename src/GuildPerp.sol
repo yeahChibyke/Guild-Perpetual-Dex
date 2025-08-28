4b387d0e05bb882ce798d2e2300a880aecf14814
yeahChibyke <chibykepride@gmail.com>
2025-08-28 22:27:38 +0100

    started work on GuildPerp

---
+++ src/GuildPerp.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract GuildPerp is ReentrancyGuard {}
