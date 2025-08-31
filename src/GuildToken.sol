6e86ac2cce6daebe5704c2502f54c9864ede9c21
yeahChibyke <chibykepride@gmail.com>
2025-08-31 06:05:11 +0100

    added headers

--- src/GuildToken.sol
+++ src/GuildToken.sol
@@ -1,50 +1,72 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
+// ------------------------------------------------------------------
+//                             IMPORTS
+// ------------------------------------------------------------------
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GuildToken is ERC20, Ownable {
+    // ------------------------------------------------------------------
+    //                              ERRORS
+    // ------------------------------------------------------------------
     error GTK__ZeroAddress();
     error GTK__NotAdmin();
     error GTK__NotVault();
     error GTK__VaultNotSet();
 
+    // ------------------------------------------------------------------
+    //                              EVENTS
+    // ------------------------------------------------------------------
     event VaultSet(address indexed vault);
 
-    address public admin;
-    address public vault;
+    // ------------------------------------------------------------------
+    //                             STORAGE
+    // ------------------------------------------------------------------
+    address private s_admin;
+    address private s_vault;
 
+    // ------------------------------------------------------------------
+    //                            MODIFIERS
+    // ------------------------------------------------------------------
     modifier onlyAdmin() {
-        if (msg.sender != admin) {
+        if (msg.sender != s_admin) {
             revert GTK__NotAdmin();
         }
         _;
     }
 
     modifier onlyVault() {
-        if (msg.sender != address(vault)) {
+        if (msg.sender != address(s_vault)) {
             revert GTK__NotVault();
         }
         _;
     }
 
+    // ------------------------------------------------------------------
+    //                            CONTRUCTOR
+    // ------------------------------------------------------------------
     constructor(address _admin) ERC20("GuildToken", "GTK") Ownable(_admin) {
         if (_admin == address(0)) {
             revert GTK__ZeroAddress();
         }
 
-        admin = _admin;
+        s_admin = _admin;
     }
+
+    // ------------------------------------------------------------------
+    //                        EXTERNAL FUNCTIONS
+    // ------------------------------------------------------------------
 
     function setVault(address _vault) external onlyAdmin {
         if (_vault == address(0)) {
             revert GTK__ZeroAddress();
         }
 
-        vault = _vault;
+        s_vault = _vault;
 
-        emit VaultSet(vault);
+        emit VaultSet(s_vault);
     }
 
     function mint(address _to, uint256 _value) external onlyVault {
@@ -53,5 +75,17 @@ contract GuildToken is ERC20, Ownable {
 
     function burn(address _from, uint256 _value) external onlyVault {
         _burn(_from, _value);
+    }
+
+    // ------------------------------------------------------------------
+    //                         GETTER FUNCTIONS
+    // ------------------------------------------------------------------
+
+    function getAdmin() external view returns (address) {
+        return s_admin;
+    }
+
+    function getVault() external view returns (address) {
+        return s_vault;
     }
 }
