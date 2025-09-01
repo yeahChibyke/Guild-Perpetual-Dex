9c6a763114c559e4bb5233bf057e9e22dfd299e8
yeahChibyke <chibykepride@gmail.com>
2025-09-01 01:18:31 +0100

    slight mods

--- script/DeployGuildPerp.s.sol
+++ script/DeployGuildPerp.s.sol
@@ -6,13 +6,11 @@ import {GuildPerp} from "../src/GuildPerp.sol";
 import {IGuildToken} from "../src/interfaces/IGuildToken.sol";
 import {IGuildVault} from "../src/interfaces/IGuildVault.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
-// import {MockWBTC} from "../test/mocks/MockWBTC.sol";
 import {MockUSDC} from "../test/mocks/MockUSDC.sol";
 
 contract DeployGuildPerp is Script {
     GuildPerp gPerp;
     HelperConfig config;
-    // MockWBTC wbtc;
     MockUSDC usdc;
 
     address token = makeAddr("Guild Token");
